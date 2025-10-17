c24ec65199cc958e34663a0b1603db3b8c277712
jomoartz <johnjohananmogbolu@gmail.com>
2025-10-17 16:06:16 +0100

    feat: typography_heading

---
+++ components/typogrphy/heading.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { Text, TextStyle, StyleSheet, TextProps } from "react-native";
+import { useTheme } from "@/context/theme";
+import { COLORS } from "@/constants/colors";
+
+type HeadingProps = TextProps & {
+  text: string;
+  style?: TextStyle;
+};
+
+const Heading: React.FC<HeadingProps> = ({ text, style, ...rest }) => {
+  const { theme } = useTheme();
+  const themeColor = theme === "dark" ? COLORS.textDark : COLORS.textLight;
+
+  return (
+    <Text
+      style={[
+        styles.heading,
+        { color: themeColor },
+        style,
+      ]}
+      {...rest}
+    >
+      {text}
+    </Text>
+  );
+};
+
+const styles = StyleSheet.create({
+  heading: {
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+});
+
+export default Heading;
